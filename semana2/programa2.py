f7a12be3f7ca2585daa17dcf20d70c9330090d20
amaydomi15200 <122801344+amaydomi15200@users.noreply.github.com>
2023-01-24 14:06:41 +0000

    concatenar variables, formatear variables e identificar errores que suceden, usar comentarios de línea

---
+++ semana2/programa2.py
@@ -0,0 +1,10 @@
+"""
+    Programa2
+    Nombre:Amayrani DB
+    Fecha: 24/01/23
+    Descripcion: concatenar variables,        formatear variables e identificar         errores que suceden 
+"""
+variable1 = "Hola " #esta defieniedo una variable de tipostring da las 
+variable2 = 619 #es una variable de tipo int
+#print(variable1 + variable2)  #suma las variables 
+print("{}{}".format(variable1,variable2))  #suma las variables y las imprime
