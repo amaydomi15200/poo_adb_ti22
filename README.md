81c78aabb9b3094a9a82db38df50a331a4133fb5
amaydomi15200 <122801344+amaydomi15200@users.noreply.github.com>
2023-01-31 05:35:41 +0000

    area y perimetro de un triangulo

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 |2|programa1.py|Comentarios multilinea y concatenacion de variables|
 |3|programa2.py|cancatenar variablesy formatearlas|
 |4|programa3.py|formatear cadenas y    operaciones aritmeticas usando el         print y el format|
+|5|programa4.py|Cambiar el tipon de dato casting|
+|6|programa5.py| area y perimetro de un triangulo|
--- semana3/programa5.py
+++ semana3/programa5.py
@@ -2,5 +2,19 @@
     Programa5
     Nombre:Amayrani DB
     Fecha: 30/01/23
-    Descripcion:Area y per
+    Descripcion:Area y perimetro de triangulo
 """
+#area
+print("Area del triangulo")
+altura = int(input("Dame la altura de tu triangulo: "))
+base = int(input("Dame la base de tu triangulo: "))
+area= (base * altura) / 2
+print(area)
+
+#perimetro
+print("Perimetro del triangulo")
+lado1 = int(input("Dame el valor del lado 1: "))
+lado2 = int(input("Dame el valor del lado 2: "))
+lado3 = int(input("Dame el valor del lado 3: "))
+perimetro = (lado1 + lado2 + lado3)
+print(perimetro)
