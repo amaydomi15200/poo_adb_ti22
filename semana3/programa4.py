b6f88cb584a295885f7b9109d304045797449f7e
amaydomi15200 <122801344+amaydomi15200@users.noreply.github.com>
2023-02-15 14:48:43 +0000

    Agregar comentarios al programa4

--- semana3/programa4.py
+++ semana3/programa4.py
@@ -4,15 +4,15 @@
     Fecha: 30/01/23
     Descripcion:Cambiar el tipon de dato casting
 """
-numero1 = input("Numero1: ")
-numero2 = 10
-suma = int(numero1) + numero2
-resta = int(numero1) - numero2
-multiplicacion = int(numero1) * numero2
-division = int(numero1) / numero2
-potencia = int(numero1)**numero2
-print(suma)
-print(resta)
-print(multiplicacion)
-print(division)
-print(potencia)
+numero1 = input("Numero1: ") #Pide el valor del numero1
+numero2 = 10 #le da valor al numero2 
+suma = int(numero1) + numero2 #Hace la suma de los numeros 1 y 2
+resta = int(numero1) - numero2 # Hace la resta de los numeros 1 y 2
+multiplicacion = int(numero1) * numero2 # Hace la multiplicacion de los numeros 1 y 2
+division = int(numero1) / numero2 # Hace la division de los numeros 1 y 2
+potencia = int(numero1)**numero2  # Saca la potencia de los numeros 1 y 2
+print(suma) # Imprime el resultado suma
+print(resta) # Imprime el resultado resta
+print(multiplicacion) # Imprime el resultado multiplicacion
+print(division) # Imprime el resultado division
+print(potencia) # Imprime el resultado de la potencia
